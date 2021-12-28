ad6f2a7590110100e3a9a32674d2fc69c0a1b66a
shouguog <38897351+shouguog@users.noreply.github.com>
2021-12-28 10:19:25 -0500

    Create cb_sniffer.sh

---
+++ mutationcall/cb_sniffer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#python cb_sniffer.py PT9_BM.bam variant_PT9_BM.txt P9_BM_bam_IGV.tsv PT9_BM
+python cb_sniffer.py ../bams/PT1_BM.bam ../variant_PT9_BM.txt PT1_BM_barcode.tsv PT1_BM_barcode
+python cb_sniffer.py ../bams/PT2_BM.bam ../variant_PT9_BM.txt PT2_BM_barcode.tsv PT2_BM_barcode
+python cb_sniffer.py ../bams/PT3_BM.bam ../variant_PT9_BM.txt PT3_BM_barcode.tsv PT3_BM_barcode
+python cb_sniffer.py ../bams/PT4_BM.bam ../variant_PT9_BM.txt PT4_BM_barcode.tsv PT4_BM_barcode
+python cb_sniffer.py ../bams/PT5_BM.bam ../variant_PT9_BM.txt PT5_BM_barcode.tsv PT5_BM_barcode
+python cb_sniffer.py ../bams/PT6_BM.bam ../variant_PT9_BM.txt PT6_BM_barcode.tsv PT6_BM_barcode
+python cb_sniffer.py ../bams/PT7_BM.bam ../variant_PT9_BM.txt PT7_BM_barcode.tsv PT7_BM_barcode
+python cb_sniffer.py ../bams/PT8_BM.bam ../variant_PT9_BM.txt PT8_BM_barcode.tsv PT8_BM_barcode
+python cb_sniffer.py ../bams/PT9_BM.bam ../variant_PT9_BM.txt PT9_BM_barcode.tsv PT9_BM_barcode
+python cb_sniffer.py ../bams/PT1_CD34.bam ../variant_PT9_BM.txt PT1_CD34_barcode.tsv PT1_CD34_barcode
+python cb_sniffer.py ../bams/PT2_CD34.bam ../variant_PT9_BM.txt PT2_CD34_barcode.tsv PT2_CD34_barcode
+python cb_sniffer.py ../bams/PT3_CD34.bam ../variant_PT9_BM.txt PT3_CD34_barcode.tsv PT3_CD34_barcode
+python cb_sniffer.py ../bams/PT4_CD34.bam ../variant_PT9_BM.txt PT4_CD34_barcode.tsv PT4_CD34_barcode
+python cb_sniffer.py ../bams/PT5_CD34.bam ../variant_PT9_BM.txt PT5_CD34_barcode.tsv PT5_CD34_barcode
+python cb_sniffer.py ../bams/PT6_CD34.bam ../variant_PT9_BM.txt PT6_CD34_barcode.tsv PT6_CD34_barcode
+python cb_sniffer.py ../bams/PT7_CD34.bam ../variant_PT9_BM.txt PT7_CD34_barcode.tsv PT7_CD34_barcode
+python cb_sniffer.py ../bams/PT8_CD34.bam ../variant_PT9_BM.txt PT8_CD34_barcode.tsv PT8_CD34_barcode
+python cb_sniffer.py ../bams/PT9_CD34.bam ../variant_PT9_BM.txt PT9_CD34_barcode.tsv PT9_CD34_barcode
+
